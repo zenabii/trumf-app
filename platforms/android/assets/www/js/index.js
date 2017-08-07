6ddc14d3c85a5cacce608bdf97bfb44398ccf9c7
Sondre Edvardsen <sondre@mystore.no>
2017-08-07 05:04:24 +0200

    asd

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -3,6 +3,34 @@ $(function() {
         $('.splash').slideUp();
         $('.registration').slideDown();
     });
+
+    // Date stuff
+    for (var i = 1; i <= 31; i++) {
+        $('select#day').append(`
+            <option value="`+i+`">`+i+`</option>
+        `);
+    }
+    for (var i = 2001; i >= 1900; i--) {
+        $('select#year').append(`
+            <option value="`+i+`">`+i+`</option>
+        `);
+    }
+
+    // Postal code
+    $('#postcode').on('keyup', function() {
+        if ($(this).val().length == 4) {
+            var postalcode = $(this).val();
+            alert('send');
+            $.ajax({
+                method: 'GET',
+                url: 'https://www.trumf.no/api/postalcode/'+postalcode
+            }).done(function(data) {
+                if (data.ValidStatus == true) {
+                    $('#city').val(data.PostCity);
+                }
+            });
+        }
+    });
 });
 
 
