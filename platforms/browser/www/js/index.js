410ebcdca7eaeea519e1c77bdd39322083978ffa
Sondre Edvardsen <sondre@mystore.no>
2017-08-14 00:08:48 +0200

    Terms and stuff

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,3 +1,8 @@
+// Globals
+var dialog = {};
+var payload = {}
+
+// Document ready
 $(function() {
     $('#to-registration').on('click', function() {
         $('.splash').slideUp();
@@ -34,21 +39,44 @@ $(function() {
     // Submit
     $('form').on('submit', function(e) {
         e.preventDefault();
-        var payload = $(this).serialize();
-        $.ajax({
-            method: 'POST',
-            url: 'https://www.trumf.no/api/trumf/becomemember',
-            dataType: 'json',
-            data: payload,
-            success: function(data) {
-                alert('200');
-            },
-            error: function(xhr, textStatus, errorThrown) {
-                console.log(xhr.responseText);
-                console.log('status: '+textStatus);
-                console.log('error: '+errorThrown);
-            }
-        });
+        payload = $(this).serialize();
+        showTerms();
     });
 
+    // Modal - terms
+    dialog = document.querySelector('dialog');
+    if (!dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+    }
+    $('#confirm-terms').on('click', function(e) {
+        e.preventDefault();
+        dialog.close();
+        alert('TODO: Submit form');
+        submitForm();
+    });
+    $('#close-terms').on('click', function(e) {
+        e.preventDefault();
+        dialog.close();
+    });
 });
+
+function showTerms() {
+    dialog.showModal();
+}
+
+function submitForm() {
+    $.ajax({
+        method: 'POST',
+        url: 'https://www.trumf.no/api/trumf/becomemember',
+        dataType: 'json',
+        data: payload,
+        success: function(data) {
+            alert('200');
+        },
+        error: function(xhr, textStatus, errorThrown) {
+            console.log(xhr.responseText);
+            console.log('status: '+textStatus);
+            console.log('error: '+errorThrown);
+        }
+    });
+}
